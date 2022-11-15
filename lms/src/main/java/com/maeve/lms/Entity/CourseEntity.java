d76646bc66f33cef2561b76e57c9a1327aafbfaa
Kathlen Claire Procorato <kathlenclaire.procorato@cit.edu>
2022-11-15 22:56:23 +0800

    Add CRUD implementation for Course

--- lms/src/main/java/com/maeve/lms/Entity/CourseEntity.java
+++ lms/src/main/java/com/maeve/lms/Entity/CourseEntity.java
@@ -1,5 +1,91 @@
 package com.maeve.lms.Entity;
 
+import javax.persistence.CascadeType;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToOne;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "tbl_course")
 public class CourseEntity {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int courseid;
+	
+	private String name;
+	private String description;
+	private int units;
+	
+	@OneToOne (cascade = CascadeType.MERGE)
+	private String teacher;
+	
+	private int numberOfStudents;
+	
+	public CourseEntity() {}
+
+	public CourseEntity(int courseid, String name, String description, int units, String teacher,
+			int numberOfStudents) {
+		super();
+		this.courseid = courseid;
+		this.name = name;
+		this.description = description;
+		this.units = units;
+		this.teacher = teacher;
+		this.numberOfStudents = numberOfStudents;
+	}
+
+	public int getCourseid() {
+		return courseid;
+	}
+
+	public void setCourseid(int courseid) {
+		this.courseid = courseid;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public int getUnits() {
+		return units;
+	}
+
+	public void setUnits(int units) {
+		this.units = units;
+	}
+
+	public String getTeacher() {
+		return teacher;
+	}
+
+	public void setTeacher(String teacher) {
+		this.teacher = teacher;
+	}
+
+	public int getNumberOfStudents() {
+		return numberOfStudents;
+	}
+
+	public void setNumberOfStudents(int numberOfStudents) {
+		this.numberOfStudents = numberOfStudents;
+	}
+	
+	
 
 }
+
--- lms/src/main/java/com/maeve/lms/Service/CourseService.java
+++ lms/src/main/java/com/maeve/lms/Service/CourseService.java
@@ -1,5 +1,66 @@
 package com.maeve.lms.Service;
 
+import java.util.List;
+import java.util.NoSuchElementException;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.maeve.lms.Entity.CourseEntity;
+import com.maeve.lms.Repository.CourseRepository;
+
+@Service
 public class CourseService {
+	
+	@Autowired
+	CourseRepository crepo;
+	
+		///Create or insert a record
+		public CourseEntity insertCourse(CourseEntity course) {
+			return crepo.save(course);
+		}
+		
+		//Read a record from the table named tbl_course
+		public List<CourseEntity> getAllCourses(){
+			return crepo.findAll();
+		}
+		
+		//Update
+		@SuppressWarnings("finally")
+		public CourseEntity putCourse(int id, CourseEntity newCourseDetails) throws Exception {
+			CourseEntity course = new CourseEntity();
+			
+			try {
+				//1 - search the id number of the course
+				course = crepo.findById(id).get();; //findById is predefined
+				
+				//2 - update the record
+				course.setName(newCourseDetails.getName());
+				course.setDescription(newCourseDetails.getDescription());
+				course.setUnits(newCourseDetails.getUnits());
+				course.setTeacher(newCourseDetails.getTeacher());
+				course.setNumberOfStudents(newCourseDetails.getNumberOfStudents());
+				
+				//3 - save the information and return the value - refer to finally block
+			}catch(NoSuchElementException nex) {
+				throw new Exception("ID Number " + id + "does not exist");
+			}finally {
+				return crepo.save(course);
+			}
+		}
+		//Delete
+		public String deleteCourse(int id) {
+			String msg;
+			if(crepo.findById(id) != null) {
+				crepo.deleteById(id);
+				
+				msg = "Course ID Number " + id + " is successfully deleted!";
+			}
+			else
+				msg = "Course ID Number " + id + " is NOT found!";	
+			return msg;
+		}
 
 }
+
+
