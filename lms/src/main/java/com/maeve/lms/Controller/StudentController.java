20d15e17f38e1ccd3ee2539093844b189eb80fd2
ROBERT WILLIAM WACAN <robertwilliam.wacan@cit.edu>
2022-11-22 20:57:11 +0800

    Add StudentController

---
+++ lms/src/main/java/com/maeve/lms/Controller/StudentController.java
@@ -0,0 +1,63 @@
+package com.maeve.lms.Controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.maeve.lms.Entity.StudentEntity;
+import com.maeve.lms.Service.StudentService;
+
+
+@RestController
+@RequestMapping("/student")
+public class StudentController {
+	
+	@Autowired
+	StudentService stserv;
+	
+	//printHello
+	@GetMapping("/print")
+	public String printHelloStudent(){
+		return "Hello, Student!";
+	}
+	
+	//Create or insert a student record
+	@PostMapping("/postStudent")
+	public StudentEntity insertStudent(@RequestBody StudentEntity student) {
+		return stserv.insertStudent(student);
+	}
+	
+	//Read
+	@GetMapping("/displayAllStudents")
+	public List<StudentEntity> getAllStudents(){
+		return stserv.getAllStudents();
+	}
+	
+	//Read a record by firstname
+	@GetMapping("/getByFirstname")
+	public StudentEntity findByFirstname(@RequestParam String firstname) {
+		return stserv.findByFirstName(firstname);
+	}
+	
+	//Update a Record
+	@PutMapping("/putStudent")
+	public StudentEntity putStudent(@RequestParam int id,@RequestBody StudentEntity newStudentDetails) throws Exception{
+		return stserv.putStudent(id, newStudentDetails);
+	}
+	
+	//Delete a record
+	@DeleteMapping("/deleteStudent/{id}")
+	public String deleteStudent(@PathVariable int id) {
+		return stserv.deleteStudent(id);
+	}
+
+}
