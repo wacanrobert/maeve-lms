84c64d5f445e23039189bb9d1480afc9827a2012
ROBERT WILLIAM WACAN <robertwilliam.wacan@cit.edu>
2022-12-02 16:15:25 +0800

    Add StudentDashboard

--- maeve_app/src/App.js
+++ maeve_app/src/App.js
@@ -5,6 +5,8 @@ import ReactDOM from 'react-dom/client';
 import StudentService from './Services/StudentService.js';
 import ReadStudents from './Component/ReadStudents';
 import Login from './Component/Login';
+import StudentDashboard from './Component/StudentDashboard';
+import Logout from './Component/Logout';
 import { BrowserRouter as Router, Routes, Route} from "react-router-dom";
 
 function App() {
@@ -12,7 +14,10 @@ function App() {
     <div className = "App">
       <Router>
         <Routes>
-          <Route exact path="/" element ={<Login/>}/>
+        <Route exact path="/" element ={<Logout/>}/>
+          <Route exact path="/Logout" element ={<Logout/>}/>
+          <Route exact path="/login" element ={<Login/>}/>
+          <Route exact path="/StudentDashboard" element ={<StudentDashboard/>}/>
         </Routes>
       </Router>
     </div>
---
+++ maeve_app/src/Component/StudentDashboard.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import StudentAppBar from './StudentAppBar';
+
+export default function StudentDashboard(){
+
+        return (
+            <div className ="App">
+                
+                <StudentAppBar/>   
+                <h1>Student Dashboard</h1>
+            </div>
+        );
+}
